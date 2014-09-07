6011cfba66f0f7968daeb38fef1e1e2c923d4794
SeedPaths-User <lessdismissive@email.com>
2014-09-07 14:58:10 -0600

    Week1CodeChallenge

--- Week1CodeChallenge/Week1CodeChallenge/Program.cs
+++ Week1CodeChallenge/Week1CodeChallenge/Program.cs
@@ -9,7 +9,8 @@ namespace Week1CodeChallenge
     class Program
     {
         static void Main(string[] args)
-        {   // #2
+        {
+            // #1
             //for loop - FizzBuzz(i) pulls from below @ Static Void.
 
             //create a loop from 0=>20
@@ -50,10 +51,10 @@ namespace Week1CodeChallenge
             {
                 Console.WriteLine(number);
             }
-            
 
-         }
-        // #3
+
+        }
+        // #2
         static void Yodaizer(string word)
         {
             string text = "I like code";
@@ -63,44 +64,62 @@ namespace Week1CodeChallenge
 
             {
                 Console.WriteLine();
-                
+                Console.ReadKey();
             }
 
 
 
         }
-        //4
+        //3
         static void TextStats(string finder)
         {
-            // Text Input and Write to Console
             string input = "Code is suhweet!";
-            Console.WriteLine(input.Length);
-            
+            {
+                Console.WriteLine(input.Length);
+
+
+
+            }
         }
-        // #5
+        // #4
         //IsPrime takes an integer parameter called "number" and performs logic
         // to determine if it is a prime number.
-        static void IsPrime(string prime)
-
+        static void IsPrime(int number)
         {
-            // #6
-            //If the number is a prime number, your function will write:
-            // "X is a prime number"
-            //If the number is not prime, print out the number.
-
-            //Create a loop from 1-25 - It calls IsPrime for each number from 1-25.
-            for (int i = 1; i <= 25; i++)
+            if (number > 1 && number % number == 0 && number % 1 == 0 && number % 3 != 0 && number % 5 != 0)
             {
-                
+                Console.WriteLine(number + " is a prime number");
             }
-                
+            else if (number == 3 || number == 5)
+            {
+                Console.WriteLine(number + " is a prime number");
+            }
+            else
+            {
+                //number is not prime
+                Console.WriteLine(number);
+            }
 
+            Console.Read();
+        }
+        // # 5
+        static void DashInsert(int number)
+        {
+            string numberString = number.ToString();
+
+            string output = "";
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                int num1 = int.Parse(numberString[i].ToString());
+                output = output + num1;
+                if (i != numberString.Length - 1 && num1 % 2 != 0 && int.Parse(numberString[i + 1].ToString()) != 0)
+                {
+                    output = output + "-";
+                }
+            }
+            // Print output
+            Console.WriteLine(output);
 
         }
-
-
-
-
     }
-
-} 
+}
